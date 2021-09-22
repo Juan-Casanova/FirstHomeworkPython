8e77fe5d93d2803552b33c8f6e03673443a8d5f2
Juan-Casanova <casanovajm199621@gmail.com>
2021-09-21 23:50:06 -0500

    finish exercise

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+import operator
 
-print("hola muncdo cruel")
+nombre = input("Cual es el nombre del archivo: ")
+listaDePalabras = []
+contadorDePalabras = {}
+list2 = {}
+
+def checadorNombre(nombre):
+    try:
+        file = open(nombre)
+        texto = file.read()
+        listaDePalabras = texto.split(" ")
+        for palabra in listaDePalabras:
+            contadorDePalabras[palabra] = contadorDePalabras.get(palabra, 0)+1
+        checadorMaxMin()
+    except:
+        name1 = input("No existe el nombre ingrese uno valido: ")
+        checadorNombre(name1)
+
+def checadorMaxMin():
+
+    list = sorted(contadorDePalabras.items(), key=lambda kv: kv[1], reverse = True)
+    for x in list:
+        if list.index(x) < 11:
+            print(x)
+
+checadorNombre(nombre)
